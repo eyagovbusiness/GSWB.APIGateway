c89a181ee695afb6853f947b857fd59cfebf3d95
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-01-26 19:05:45 +0100

    GSWB-83 Updated dockerfile and local deploy script to use ENVIRONMENT arg used in the tag full name.

--- local_deploy.sh
+++ local_deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eux
 
-DockerVersion=1.0.0
+Environment=development
 
 # tarball csproj files, sln files, and NuGet.config
 find . \( -name "*.csproj" -o -name "*.sln" -o -name "NuGet.docker.config" \) -print0 \
     | tar -cvf projectfiles.tar --null -T -
 
-docker build . -t registry.guildswarm.org/backend-images/api-gateway:$DockerVersion -t registry.guildswarm.org/backend-images/api-gateway:latest
+docker build . --build-arg ENVIRONMENT=$Environment -t registry.guildswarm.org/$Environment/api_gateway:latest
 if [[ $0 == 1 || 0 ]]
 then
     rm projectfiles.tar
