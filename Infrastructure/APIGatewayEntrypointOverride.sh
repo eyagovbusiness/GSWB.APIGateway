902c4dd62e255d42ec4c19c171b99784611af524
retributter <alejandrobinarycode@gmail.com>
2024-06-09 12:19:16 +0200

    GSWI-95: Adapting code to the new version without checkings

--- Infrastructure/APIGatewayEntrypointOverride.sh
+++ Infrastructure/APIGatewayEntrypointOverride.sh
@@ -9,19 +9,9 @@ set -e
 # "import" service await functions
 source wait_for_service.sh
 
-# Execute tasks before the base entrypoint starts
-execute_before_start() {
-    echo "Executing scheduled tasks before the base entrypoint starts.."	
-	wait_IsReady vault.vault.svc.cluster.local #vault waits consul is ready :)
-	echo "Scheduled tasks before the base entrypoint starts..DONE."
-}
-
 # Main function that orchestrates the execution of the script, executing first the function with the custom logic that should be executed before calling the base entrypoint. 
 # Secondly calls the base entrypoint and finally executed the function with the custom logic that should be executed after the base entripoint is called.
 main() {
-    echo "Starting entrypoint override" 
-    execute_before_start
-
     echo "Calling base Entrypoint"
     #dotnet APIGateway.API.dll &
 	./APIGateway.API &
