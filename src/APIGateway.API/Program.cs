1d565945e3527a446a180ad653f64e692ab3e0f2
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-08-11 20:11:30 +0200

    Added UseForwardedHeaders and UseHttpsRedirection

--- src/APIGateway.API/Program.cs
+++ src/APIGateway.API/Program.cs
@@ -2,6 +2,7 @@ using APIGateway.API;
 using APIGateway.Application;
 using APIGateway.Infrastructure;
 using Common.Presentation;
+using Microsoft.AspNetCore.HttpOverrides;
 
 
 WebApplicationBuilder lAPIGatewayApplicationBuilder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,13 @@ lAPIGatewayApplicationBuilder.ConfigureCommonPresentation();
 lAPIGatewayApplicationBuilder.ConfigurePresentation();
 
 var lAPIGatewayWebApplication = lAPIGatewayApplicationBuilder.Build();
+lAPIGatewayWebApplication.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
+// Configurar el middleware para redirigir HTTP a HTTPS
+lAPIGatewayWebApplication.UseHttpsRedirection();
 await lAPIGatewayWebApplication.UseInfrastructure();
 lAPIGatewayWebApplication.UsePresentation();
 
